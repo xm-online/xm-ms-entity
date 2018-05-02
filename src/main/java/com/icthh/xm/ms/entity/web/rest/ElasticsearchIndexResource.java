06c56ebd4eecc7b65c37fdcd05e5876fcd04551a
emonastyrev <emonastyrev@jbs.com.ua>
2018-05-02 13:33:54 +0300

    add permission to index

--- src/main/java/com/icthh/xm/ms/entity/web/rest/ElasticsearchIndexResource.java
+++ src/main/java/com/icthh/xm/ms/entity/web/rest/ElasticsearchIndexResource.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +29,7 @@ public class ElasticsearchIndexResource {
      */
     @PostMapping("/elasticsearch/index")
     @Timed
-    @Secured(RoleConstant.SUPER_ADMIN)
+    @PreAuthorize("hasPermission(null, 'ELASTICSEARCH.INDEX')")
     public ResponseEntity<Void> reindexAll() {
         elasticsearchIndexService.reindexAll();
         return ResponseEntity.accepted()
