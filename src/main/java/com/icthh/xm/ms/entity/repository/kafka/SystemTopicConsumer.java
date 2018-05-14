2be5ea50360abf47731b966a8f7c8a0e2bc0b00b
emonastyrev <emonastyrev@jbs.com.ua>
2018-05-14 11:03:56 +0300

    remove topic

--- src/main/java/com/icthh/xm/ms/entity/repository/kafka/SystemTopicConsumer.java
+++ src/main/java/com/icthh/xm/ms/entity/repository/kafka/SystemTopicConsumer.java
@@ -1,34 +1,21 @@
 package com.icthh.xm.ms.entity.repository.kafka;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.icthh.xm.commons.config.client.repository.ConfigurationModel;
-import com.icthh.xm.commons.config.domain.Configuration;
 import com.icthh.xm.commons.logging.util.MdcUtils;
-import com.icthh.xm.commons.messaging.event.system.SystemEvent;
-import com.icthh.xm.commons.messaging.event.system.SystemEventType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.util.Objects;
-import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class SystemTopicConsumer {
 
-    private final Optional<ConfigurationModel> configurationModel;
-
     /**
      * Consume tenant command event message.
+     *
      * @param message the tenant command event message
      */
     @Retryable(maxAttemptsExpression = "${application.retry.max-attempts}",
@@ -38,41 +25,8 @@ public class SystemTopicConsumer {
         MdcUtils.putRid();
         try {
             log.info("Consume event from topic [{}]", message.topic());
-            ObjectMapper mapper = new ObjectMapper()
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-            mapper.registerModule(new JavaTimeModule());
-            try {
-                SystemEvent event = mapper.readValue(message.value(), SystemEvent.class);
-
-                log.info("Process event from topic [{}], type='{}', source='{}', event_id ='{}'",
-                    message.topic(), event.getEventType(), event.getMessageSource(), event.getEventId());
-
-                switch (event.getEventType().toUpperCase()) {
-                    case SystemEventType.SAVE_CONFIGURATION:
-                        onSaveConfiguration(event);
-                        break;
-                    default:
-                        log.info("Event ignored with type='{}', source='{}', event_id='{}'",
-                            event.getEventType(), event.getMessageSource(), event.getEventId());
-                        break;
-                }
-            } catch (IOException e) {
-                log.error("System queue message has incorrect format: '{}'", message.value(), e);
-            }
         } finally {
             MdcUtils.removeRid();
         }
-    }
-
-    private void onSaveConfiguration(SystemEvent event) {
-        String path = Objects.toString(event.getDataMap().get("path"), null);
-        if (StringUtils.isBlank(path)) {
-            throw new IllegalArgumentException("Event '" + event.getEventType() + "' configuration path can't be blank");
-        }
-        String commit = Objects.toString(event.getDataMap().get("commit"), null);
-        if (StringUtils.isBlank(commit)) {
-            throw new IllegalArgumentException("Event '" + event.getEventType() + "' configuration commit can't be blank");
-        }
-        configurationModel.ifPresent(model -> model.updateConfiguration(new Configuration(path, null, commit)));
     }
 }
