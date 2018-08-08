a6c6cc87f87ebf0cf5c77682d4b7ae89def5298a
ssenko <sergeysenja1992@gmail.com>
2018-08-08 12:16:13 +0300

    get function

--- src/main/java/com/icthh/xm/ms/entity/web/rest/FunctionResource.java
+++ src/main/java/com/icthh/xm/ms/entity/web/rest/FunctionResource.java
@@ -4,13 +4,16 @@ import com.codahale.metrics.annotation.Timed;
 import com.icthh.xm.ms.entity.domain.FunctionContext;
 import com.icthh.xm.ms.entity.service.FunctionService;
 import com.icthh.xm.ms.entity.web.rest.util.HeaderUtil;
+import com.icthh.xm.ms.entity.web.rest.util.RespContentUtil;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -30,6 +33,15 @@ public class FunctionResource {
 
     public FunctionResource(@Qualifier("functionService") FunctionService functionService) {
         this.functionService = functionService;
+    }
+
+    @Timed
+    @GetMapping("/functions/{functionKey:.+}")
+    @PreAuthorize("hasPermission(null, 'FUNCTION.GET.CALL')")
+    public ResponseEntity<FunctionContext> callGetFunction(@PathVariable("functionKey") String functionKey,
+                                                           @RequestParam(required = false) Map<String, Object> functionInput) {
+        FunctionContext result = functionService.execute(functionKey, functionInput);
+        return ResponseEntity.ok().body(result);
     }
 
     /**
---
+++ src/test/java/com/icthh/xm/ms/entity/web/rest/FunctionResourceMvcIntTest.java
@@ -0,0 +1,69 @@
+package com.icthh.xm.ms.entity.web.rest;
+
+import static com.google.common.collect.ImmutableMap.of;
+import static java.time.Instant.now;
+import static org.hamcrest.Matchers.hasItem;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.refEq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.google.common.collect.ImmutableMap;
+import com.icthh.xm.commons.exceptions.spring.web.ExceptionTranslator;
+import com.icthh.xm.ms.entity.domain.FunctionContext;
+import com.icthh.xm.ms.entity.service.FunctionService;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.Instant;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = FunctionResource.class)
+@ContextConfiguration(classes = {FunctionResource.class, ExceptionTranslator.class})
+public class FunctionResourceMvcIntTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @MockBean(name = "functionService")
+    private FunctionService functionService;
+
+    @Before
+    public void setup() {
+        // Setup MockMVC to use our Spring Configuration
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    @SneakyThrows
+    public void testCallGetFunction() {
+        when(functionService.execute("SOME-FUNCTION_KEY.TROLOLO", of("var1", "val1", "var2", "val2")))
+            .thenReturn(new FunctionContext().data(of("test", "result")));
+        mockMvc.perform(get("/api/functions/SOME-FUNCTION_KEY.TROLOLO?var1=val1&var2=val2"))
+            .andDo(print())
+            .andExpect(jsonPath("$.data.test").value("result"))
+            .andExpect(status().isOk());
+        verify(functionService).execute(eq("SOME-FUNCTION_KEY.TROLOLO"), eq(of("var1", "val1", "var2", "val2")));
+    }
+
+}
