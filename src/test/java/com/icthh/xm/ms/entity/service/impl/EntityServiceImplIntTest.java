dcbdcd9cc1b0ffe4b40d0b1ce168584d9953b37a
ssenko </dev/null>
2018-08-19 15:02:06 +0300

    Fix tests

--- src/test/java/com/icthh/xm/ms/entity/service/impl/EntityServiceImplIntTest.java
+++ src/test/java/com/icthh/xm/ms/entity/service/impl/EntityServiceImplIntTest.java
@@ -1,6 +1,5 @@
 package com.icthh.xm.ms.entity.service.impl;
 
-import static com.google.common.collect.ImmutableMap.of;
 import static com.icthh.xm.commons.lep.XmLepConstants.THREAD_CONTEXT_KEY_AUTH_CONTEXT;
 import static com.icthh.xm.commons.lep.XmLepConstants.THREAD_CONTEXT_KEY_TENANT_CONTEXT;
 import static com.icthh.xm.commons.tenant.TenantContextUtils.getRequiredTenantKeyValue;
