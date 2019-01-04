c40eec3c690b2cd37156e83bb795fa6ce7db2ed6
ssenko <sergeysenja1992@gmail.com>
2019-01-04 13:18:14 +0200

    Add test

---
+++ src/test/resources/config/testlep/SchedulerEvent$$TEST_TYPE_KEY$$around.groovy
@@ -0,0 +1,2 @@
+lepContext.inArgs.scheduledEvent.data.countCallEventHandler = lepContext.inArgs.scheduledEvent.data.countCallEventHandler + 1
+lepContext.inArgs.scheduledEvent.data.scheduledEvent = lepContext.inArgs.scheduledEvent.toString()
