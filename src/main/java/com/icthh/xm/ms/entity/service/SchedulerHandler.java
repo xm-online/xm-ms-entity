0b0e0559acd4b6cdfef486f48b5b71976ffcd464
ssenko <sergeysenja1992@gmail.com>
2019-01-08 23:32:50 +0200

    fix scheduler

--- src/main/java/com/icthh/xm/ms/entity/service/SchedulerHandler.java
+++ src/main/java/com/icthh/xm/ms/entity/service/SchedulerHandler.java
@@ -44,16 +44,11 @@ public class SchedulerHandler implements SchedulerEventHandler {
             threadContext.setValue(THREAD_CONTEXT_KEY_AUTH_CONTEXT, authContextHolder.getContext());
         });
 
-        String newRid = MdcUtils.getRid()
-            + ":" + StringUtils.defaultIfBlank(login, "")
-            + ":" + StringUtils.defaultIfBlank(tenantKey, "");
-        MdcUtils.putRid(newRid);
     }
 
     private void destroy() {
         lepManager.endThreadContext();
         tenantContextHolder.getPrivilegedContext().destroyCurrentContext();
-        MdcUtils.removeRid();
     }
 
 }
