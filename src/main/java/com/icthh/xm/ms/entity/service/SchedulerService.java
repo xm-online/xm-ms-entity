f39f2ba80fd68711358441a640cb8ca8ec7ecd6d
ssenko <sergeysenja1992@gmail.com>
2019-01-08 16:21:47 +0200

    update scheduler

--- src/main/java/com/icthh/xm/ms/entity/service/SchedulerService.java
+++ src/main/java/com/icthh/xm/ms/entity/service/SchedulerService.java
@@ -6,9 +6,11 @@ import com.icthh.xm.commons.scheduler.domain.ScheduledEvent;
 import com.icthh.xm.ms.entity.lep.keyresolver.SchedulerEventTypeKeyResolver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
+@Transactional
 @LepService(group = "scheduler")
 public class SchedulerService {
 
