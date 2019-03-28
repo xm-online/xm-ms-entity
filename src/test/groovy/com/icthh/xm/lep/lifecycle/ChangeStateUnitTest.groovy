17e7cf221a82fbe5a26ff9800001eee617268700
amedvedchuk <amedvedchuk@jbs.com.ua>
2019-03-28 16:07:25 +0200

    Refactor groovy abstract test and add LepTestLinkScanner

--- src/test/groovy/com/icthh/xm/lep/lifecycle/ChangeStateUnitTest.groovy
+++ src/test/groovy/com/icthh/xm/lep/lifecycle/ChangeStateUnitTest.groovy
@@ -6,7 +6,6 @@ import com.icthh.xm.ms.entity.domain.XmEntity
 import com.icthh.xm.ms.entity.domain.ext.IdOrKey
 import com.icthh.xm.ms.entity.service.XmEntityService
 import com.icthh.xm.ms.entity.service.XmTenantLifecycleService
-import org.junit.Before
 import org.junit.Test
 
 import static com.icthh.xm.lep.LepTestConstants.LEP_DEFAULT_PATH
@@ -16,41 +15,37 @@ class ChangeStateUnitTest extends AbstractGroovyUnitTest {
 
     String scriptName = LEP_DEFAULT_PATH + '/lifecycle/ChangeState.groovy'
 
-    GroovyShell groovyShell
-    Binding binding
-    def lepContext
-
-    @Before
-    void setUp() {
-        groovyShell = new GroovyShell()
-        binding = new Binding()
-        lepContext = [
-            inArgs:[
-                idOrKey: null,
+    @Override
+    Object setLepContext() {
+        [
+            inArgs  : [
+                idOrKey     : null,
                 nextStateKey: null,
-                context: [:]
+                context     : [:]
             ],
-            services:[
-                xmEntity: null,
+            services: [
+                xmEntity         : null,
                 xmTenantLifeCycle: null
             ],
-            commons:[
+            commons : [
             ]
         ]
     }
 
+    private <T> T evaluateScript() {
+        evaluateScript(scriptName)
+    }
+
     @Test
     void testErrorOnNotFound() {
-        def msg = shouldFail(EntityNotFoundException) {
-            lepContext.inArgs.idOrKey = IdOrKey.of(1)
-            lepContext.services.xmEntity = [
-                findOne : { IdOrKey key -> null }] as XmEntityService
 
-            //set context
-            groovyShell.setVariable('lepContext', lepContext)
-            groovyShell.evaluate(new File(scriptName))
+        lepContext.inArgs.idOrKey = IdOrKey.of(1)
+        lepContext.services.xmEntity = [findOne: { IdOrKey key -> null }] as XmEntityService
+
+        EntityNotFoundException ex = shouldFail(EntityNotFoundException) {
+            evaluateScript()
         }
-        assertEquals 'Entity not found', msg
+        assert 'Entity not found' == ex.message
     }
 
     @Test
@@ -58,19 +53,17 @@ class ChangeStateUnitTest extends AbstractGroovyUnitTest {
         lepContext.inArgs.idOrKey = IdOrKey.of(1)
         lepContext.inArgs.nextStateKey = "STATE1"
         lepContext.services.xmEntity = [
-            findOne : { IdOrKey key -> newXmEntity(IdOrKey.of(1), "ENY_TYPE", [:]) },
-            save: { XmEntity entity -> entity}
+            findOne: { IdOrKey key -> newXmEntity(IdOrKey.of(1), "ENY_TYPE", [:]) },
+            save   : { XmEntity entity -> entity }
         ] as XmEntityService
 
-        //set context
-        groovyShell.setVariable('lepContext', lepContext)
-        XmEntity value = groovyShell.evaluate(new File(scriptName))
+        XmEntity value = evaluateScript()
 
         XmEntity ent = newXmEntity(IdOrKey.of(1), "ENY_TYPE", [:])
         ent.setStateKey("STATE1")
 
-        assertEquals  ent, value
-        assertEquals ent.stateKey, value.stateKey
+        assert ent == value
+        assert ent.stateKey == value.stateKey
     }
 
     @Test
@@ -79,27 +72,26 @@ class ChangeStateUnitTest extends AbstractGroovyUnitTest {
         lepContext.inArgs.nextStateKey = "STATE1"
         lepContext.inArgs.context = [:]
         lepContext.services.xmEntity = [
-            findOne : { IdOrKey key -> newXmEntity(IdOrKey.of(1), "RESOURCE.XM-TENANT", [:]) },
-            save: { XmEntity entity -> entity}
+            findOne: { IdOrKey key -> newXmEntity(IdOrKey.of(1), "RESOURCE.XM-TENANT", [:]) },
+            save   : { XmEntity entity -> entity }
         ] as XmEntityService
 
         XmTenantLifecycleService xmTenantLifeCycle = mock(XmTenantLifecycleService.class)
-        doNothing().when(xmTenantLifeCycle).changeState(any(), any(), any())
-
+        doNothing().when(xmTenantLifeCycle).changeState(any(XmEntity), anyString(), anyMap())
         lepContext.services.xmTenantLifeCycle = xmTenantLifeCycle
 
-        //set context
-        groovyShell.setVariable('lepContext', lepContext)
-        XmEntity value = groovyShell.evaluate(new File(scriptName))
+        XmEntity value = evaluateScript()
 
-        XmEntity ent = newXmEntity(IdOrKey.of(1), "RESOURCE.XM-TENANT", [:])
-        ent.setStateKey("NEW")
+        verify(xmTenantLifeCycle).changeState([id: 1] as XmEntity, 'STATE1', [:])
 
-        assertEquals  ent, value
-        assertEquals ent.stateKey, value.stateKey
+        XmEntity expected = newXmEntity(IdOrKey.of(1), "RESOURCE.XM-TENANT", [:])
+        expected.setStateKey("NEW")
+
+        assert expected == value
+        assert expected.stateKey == value.stateKey
     }
 
-    private newXmEntity(IdOrKey id, String typeKey, Map data) {
+    private static newXmEntity(IdOrKey id, String typeKey, Map data) {
         XmEntity e = new XmEntity()
         e.setId(id.getId())
         e.setKey(id.getKey())
