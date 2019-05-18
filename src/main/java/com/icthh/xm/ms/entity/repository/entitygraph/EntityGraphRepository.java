8a55e0773e5d36b922438a6dca7fe1c503eff8f9
ssenko <sergeysenja1992@gmail.com>
2019-05-18 18:20:41 +0300

    remove unused imports

--- src/main/java/com/icthh/xm/ms/entity/repository/entitygraph/EntityGraphRepository.java
+++ src/main/java/com/icthh/xm/ms/entity/repository/entitygraph/EntityGraphRepository.java
@@ -1,11 +1,6 @@
 package com.icthh.xm.ms.entity.repository.entitygraph;
 
 import com.icthh.xm.ms.entity.domain.XmEntity;
-import org.apache.poi.ss.formula.functions.T;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
