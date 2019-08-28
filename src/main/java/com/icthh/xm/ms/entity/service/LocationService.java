a4989f09eecd6ac74c34b440fe391091404f23ea
ichupryna <ichupryna@gmail.com>
2019-08-28 10:52:23 +0300

    Feature: added LEP location service

--- src/main/java/com/icthh/xm/ms/entity/service/LocationService.java
+++ src/main/java/com/icthh/xm/ms/entity/service/LocationService.java
@@ -1,5 +1,7 @@
 package com.icthh.xm.ms.entity.service;
 
+import com.icthh.xm.commons.lep.LogicExtensionPoint;
+import com.icthh.xm.commons.lep.spring.LepService;
 import com.icthh.xm.commons.permission.annotation.FindWithPermission;
 import com.icthh.xm.commons.permission.repository.PermittedRepository;
 import com.icthh.xm.ms.entity.domain.Location;
@@ -12,9 +14,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
+@LepService(group = "service.location")
 @Transactional
 @RequiredArgsConstructor
 public class LocationService {
@@ -27,7 +31,14 @@ public class LocationService {
 
     private final XmEntityRepository xmEntityRepository;
 
+    @Transactional(readOnly = true)
+    public Optional<Location> findById(Long id) {
+        return locationRepository.findById(id);
+    }
+
     @FindWithPermission("LOCATION.GET_LIST")
+    @LogicExtensionPoint("FindAll")
+    @Transactional(readOnly = true)
     public List<Location> findAll(String privilegeKey) {
         return permittedRepository.findAll(Location.class, privilegeKey);
     }
@@ -51,9 +62,21 @@ public class LocationService {
      * @param location the location to save
      * @return the persisted XmEntity
      */
+    @LogicExtensionPoint("Save")
     public Location save(Location location) {
         log.debug("Request to save location : {}", location);
         location.setXmEntity(xmEntityRepository.getOne(location.getXmEntity().getId()));
         return locationRepository.save(location);
+    }
+
+    /**
+     * Delete location by ID
+     *
+     * @param locationId
+     */
+    @LogicExtensionPoint("Delete")
+    public void delete(long locationId) {
+        log.debug("Request to delete location : {}", locationId);
+        locationRepository.deleteById(locationId);
     }
 }
