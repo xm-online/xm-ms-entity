19ba3bb0ad358c645bb210828a29b39d5aa32e43
Anton <fishing06656@gmail.com>
2020-08-03 11:37:17 +0300

    Several application peroperties beans

--- src/main/java/com/icthh/xm/ms/entity/config/filestorage/MinioStorageConfiguration.java
+++ src/main/java/com/icthh/xm/ms/entity/config/filestorage/MinioStorageConfiguration.java
@@ -2,19 +2,14 @@ package com.icthh.xm.ms.entity.config.filestorage;
 
 import com.icthh.xm.ms.entity.config.ApplicationProperties;
 import io.minio.MinioClient;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 
 @Configuration
-@RequiredArgsConstructor(onConstructor=@__(@Lazy))
 public class MinioStorageConfiguration {
 
-    private final ApplicationProperties applicationProperties;
-
     @Bean
-    public MinioClient minioClient() {
+    public MinioClient minioClient(ApplicationProperties applicationProperties) {
         ApplicationProperties.FileStorage.Minio minio = applicationProperties.getFileStorage().getMinio();
         return MinioClient.builder()
             .endpoint(minio.getHostname(), minio.getPort(), false)
