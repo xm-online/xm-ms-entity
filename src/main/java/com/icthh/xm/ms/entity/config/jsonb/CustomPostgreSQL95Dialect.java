2c46c4005f5161a2c5031f56924de032f10c532d
yaroslav.piven <yaroslav.piven@jevera.software>
2021-12-06 10:11:22 +0200

    Json extract path text

--- src/main/java/com/icthh/xm/ms/entity/config/jsonb/CustomPostgreSQL95Dialect.java
+++ src/main/java/com/icthh/xm/ms/entity/config/jsonb/CustomPostgreSQL95Dialect.java
@@ -9,6 +9,9 @@ public class CustomPostgreSQL95Dialect extends FixedPostgreSQL95Dialect {
     public static final String JSON_QUERY_TEMPLATE = "jsonb_path_query_first(?1, ?2::jsonpath)";
     public static final String JSON_QUERY = "json_query";
 
+    public static final String JSON_EXTRACT_PATH_TEXT = "jsonb_extract_path_text(?1, ?2)";
+    public static final String JSON_EXTRACT_PATH_TEXT_TEMPLATE = "jsonb_to_string";
+
     public static final String TO_JSON_B = "to_json_b";
     public static final String TO_JSON_B_TEMPLATE = "to_jsonb(?1)";
 
@@ -16,6 +19,7 @@ public class CustomPostgreSQL95Dialect extends FixedPostgreSQL95Dialect {
         super();
         registerFunction(JSON_QUERY, new SQLFunctionTemplate(StringType.INSTANCE, JSON_QUERY_TEMPLATE));
         registerFunction(TO_JSON_B, new SQLFunctionTemplate(StringType.INSTANCE, TO_JSON_B_TEMPLATE));
+        registerFunction(JSON_EXTRACT_PATH_TEXT, new SQLFunctionTemplate(StringType.INSTANCE, JSON_EXTRACT_PATH_TEXT_TEMPLATE));
     }
 
 }
