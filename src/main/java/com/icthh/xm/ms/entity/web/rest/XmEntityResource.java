89c340a328c621d24b512c24ef69011d0c7de526
Serhii Senko <serhii.senko@jevera.software>
2021-12-07 19:11:31 +0200

    add logging

--- src/main/java/com/icthh/xm/ms/entity/web/rest/XmEntityResource.java
+++ src/main/java/com/icthh/xm/ms/entity/web/rest/XmEntityResource.java
@@ -29,6 +29,7 @@ import com.icthh.xm.ms.entity.web.rest.util.HeaderUtil;
 import com.icthh.xm.ms.entity.web.rest.util.PaginationUtil;
 import com.icthh.xm.ms.entity.web.rest.util.RespContentUtil;
 import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
@@ -67,6 +68,7 @@ import javax.validation.Valid;
 /**
  * REST controller for managing XmEntity.
  */
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class XmEntityResource {
@@ -234,7 +236,7 @@ public class XmEntityResource {
             @RequestParam String query,
             @ApiParam Pageable pageable) {
         Page<XmEntity> page = xmEntityService.search(query, pageable, null);
-        log.info("Result of search in web {}", page.getContent())
+        log.info("Result of search in web {}", page.getContent());
         HttpHeaders headers = PaginationUtil
             .generateSearchPaginationHttpHeaders(query, page,
                 "/api/_search/xm-entities");
