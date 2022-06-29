969b45a1656d5b34ba375c39427638597f885cc1
omoldavskyi <oleksandr.moldavskyi@jevera.software>
2022-06-29 18:37:52 +0300

    add test for CriteriaBuilder FindAll by fields

--- src/test/java/com/icthh/xm/ms/entity/lep/LepEntityRepositoryIntTest.java
+++ src/test/java/com/icthh/xm/ms/entity/lep/LepEntityRepositoryIntTest.java
@@ -13,19 +13,23 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.transaction.BeforeTransaction;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.shaded.org.apache.commons.io.IOUtils;
 
+import java.io.InputStream;
+import java.util.List;
 import java.util.Map;
 
 import static com.icthh.xm.commons.lep.XmLepConstants.THREAD_CONTEXT_KEY_AUTH_CONTEXT;
 import static com.icthh.xm.commons.lep.XmLepConstants.THREAD_CONTEXT_KEY_TENANT_CONTEXT;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-
 @Transactional
-public class LepContextCastIntTest extends AbstractSpringBootTest {
-
+public class LepEntityRepositoryIntTest extends AbstractSpringBootTest  {
     @Autowired
     private TenantContextHolder tenantContextHolder;
 
@@ -66,15 +70,23 @@ public class LepContextCastIntTest extends AbstractSpringBootTest {
     @Test
     @Transactional
     @SneakyThrows
-    public void testLepContextCast() {
+    public void testFindAllByFields() {
         String functionPrefix = "/config/tenants/RESINTTEST/entity/lep/function/";
-        String functionKey = "LEP-CONTEXT-TEST";
-        String funcKey = functionPrefix + "Function$$LEP_CONTEXT_TEST$$tenant.groovy";
-        String function = "import com.icthh.xm.ms.entity.lep.LepContext;\nLepContext context = lepContext\nreturn ['context':context]";
+        String functionKey = "FIND-ALL-BY-FIELDS";
+        String funcKey = functionPrefix + "Function$$FIND_ALL_BY_FIELDS$$tenant.groovy";
+
+        InputStream cfgInputStream = new ClassPathResource("config/testlep/Function$$FIND_All_BY_FIELDS$$around.groovy").getInputStream();
+        String function = IOUtils.toString(cfgInputStream, UTF_8);
+
         leps.onRefresh(funcKey, function);
         Map<String, Object> result = functionExecutorService.execute(functionKey, Map.of(), null);
-        assertTrue(result.get("context") instanceof LepContext);
+        List<Map<String, Object>> resultEntities = (List) result.get("results");
+
+        assertEquals(1, resultEntities.size());
+        assertEquals(2, resultEntities.get(0).size());
+        assertEquals("TEST_EXPORT_2", resultEntities.get(0).get("typeKey"));
+        assertTrue(resultEntities.get(0).get("id") instanceof Long);
+
         leps.onRefresh(funcKey, null);
     }
-
 }
--- src/test/resources/config/testlep/Function$$FIND_All_BY_FIELDS$$around.groovy
+++ src/test/resources/config/testlep/Function$$FIND_All_BY_FIELDS$$around.groovy
@@ -1,53 +1,51 @@
 package com.icthh.xm.ms.entity.domain;
 
-import com.icthh.xm.ms.entity.domain.XmEntity
-import com.icthh.xm.ms.entity.domain.XmEntity_
-import lombok.EqualsAndHashCode
-import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.domain.Specification
 
+import javax.persistence.Tuple
 import javax.persistence.criteria.Root
 
 import static java.time.Instant.now
 import static java.util.UUID.randomUUID
-import static org.springframework.data.domain.Sort.Direction.ASC
+
+def TYPE_KEY = 'TEST_EXPORT_2'
 
 def NAME1 = 'Name1'
 def NAME2 = 'Name2'
 def NAME3 = 'Name3'
 
-
 def entityRepository = lepContext.repositories.xmEntity
 
-
 def entities = []
-entities << createEntity('TEST_EXPORT_2', NAME1)
-entities << createEntity('TEST_EXPORT_2', NAME2)
-entities << createEntity('TEST_EXPORT_2', NAME3)
+entities << createEntity(TYPE_KEY, NAME1)
+entities << createEntity(TYPE_KEY, NAME2)
+entities << createEntity(TYPE_KEY, NAME3)
 entityRepository.saveAll(entities)
 
-List<?> results = entityRepository.findAll(Specification.where({ root, query, cb ->
+List<Tuple> foundResults = entityRepository.findAll(Specification.where({ root, query, cb ->
     return cb.and(
-        cb.equal(root.get('name'), NAME1)
-    )
+        cb.equal(root.get('typeKey'), TYPE_KEY),
+        cb.equal(root.get('name'), NAME2))
 }), { Root<XmEntity> root ->
-    return [root.get(XmEntity_.typeKey), root.get(XmEntity_.id)]
+    return [
+        root.get(XmEntity_.typeKey).alias('typeKey'),
+        root.get(XmEntity_.id).alias('id')
+    ]
 }, PageRequest.of(0, 10))
 
-return [foundEntities: results.collect { ['id': it.id, 'typeKey': it.typeKey] }]
-
-@EqualsAndHashCode
-class ProjectionClass {
-    String typeKey;
-    String name;
-
-    ProjectionClass(String typeKey, String name) {
-        this.typeKey = typeKey
-        this.name - name
+def results = []
+foundResults.each {tuple ->
+    def elements = tuple.getElements()
+    Map<String, Object> mappedResult = [:]
+    elements.each {tupleElement ->
+        String name = tupleElement.getAlias();
+        mappedResult.put(name, tuple.get(name));
     }
+    results << mappedResult
 }
+
+return [results: results]
 
 def createEntity(String typeKey, String name) {
     return new XmEntity()
