924eefbfde20f153c22a5982be10b33c9524cbd1
bvolokhenko <bohdan.volokhonenko@jevera.software>
2022-09-16 12:32:45 +0300

    32953-Removed unused import

--- src/main/java/com/icthh/xm/ms/entity/service/processor/FormSpecProcessor.java
+++ src/main/java/com/icthh/xm/ms/entity/service/processor/FormSpecProcessor.java
@@ -22,7 +22,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
