71eb01e56b9ca420f38afe20ea026f72bb6f81b4
Serhii Senko <serhii.senko@jevera.software>
2022-12-24 23:29:06 +0200

    Fix tests

--- src/main/java/com/icthh/xm/ms/entity/lep/helpers/LepDataClassTransformation.java
+++ src/main/java/com/icthh/xm/ms/entity/lep/helpers/LepDataClassTransformation.java
@@ -1,6 +1,10 @@
 package com.icthh.xm.ms.entity.lep.helpers;
 
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import lombok.SneakyThrows;
+import org.apache.commons.io.IOUtils;
 import org.codehaus.groovy.ast.ASTNode;
 import org.codehaus.groovy.ast.ClassNode;
 import org.codehaus.groovy.ast.builder.AstBuilder;
@@ -9,6 +13,7 @@ import org.codehaus.groovy.transform.AbstractASTTransformation;
 import org.codehaus.groovy.transform.GroovyASTTransformation;
 
 import static com.icthh.xm.ms.entity.util.ResourceUtils.getResourceAsStr;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.codehaus.groovy.control.CompilePhase.CANONICALIZATION;
 import static org.codehaus.groovy.control.CompilePhase.SEMANTIC_ANALYSIS;
 
@@ -16,10 +21,12 @@ import static org.codehaus.groovy.control.CompilePhase.SEMANTIC_ANALYSIS;
 public class LepDataClassTransformation extends AbstractASTTransformation {
 
     @Override
+    @SneakyThrows
     public void visit(ASTNode[] nodes, SourceUnit source) {
         ClassNode classNode = (ClassNode) nodes[1];
 
-        String mockClass = getResourceAsStr("lep/DataClassTemplate.groovy");
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("lep/DataClassTemplate.groovy");
+        String mockClass = IOUtils.toString(resourceAsStream, UTF_8);
         var ast = new AstBuilder().buildFromString(CANONICALIZATION, false, mockClass);
         ClassNode mockClassNode = (ClassNode) ast.get(1);
         mockClassNode.getFields().forEach(classNode::addField);
