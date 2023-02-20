344114000a636b1b8eb5c3debaab7148afdd4c35
ichupryna <ichupryna@gmail.com>
2023-02-20 13:28:27 +0200

    Fix ElasticsearchIndexServiceUnitTest injection

--- src/test/java/com/icthh/xm/ms/entity/service/ElasticsearchIndexServiceUnitTest.java
+++ src/test/java/com/icthh/xm/ms/entity/service/ElasticsearchIndexServiceUnitTest.java
@@ -3,31 +3,31 @@ package com.icthh.xm.ms.entity.service;
 import com.icthh.xm.commons.tenant.PlainTenant;
 import com.icthh.xm.commons.tenant.Tenant;
 import com.icthh.xm.commons.tenant.TenantContext;
-import com.icthh.xm.commons.tenant.TenantKey;
-import com.icthh.xm.ms.entity.service.search.ElasticsearchTemplateWrapper;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import com.icthh.xm.commons.tenant.TenantContextHolder;
+import com.icthh.xm.commons.tenant.TenantKey;
 import com.icthh.xm.ms.entity.AbstractUnitTest;
 import com.icthh.xm.ms.entity.config.IndexConfiguration;
 import com.icthh.xm.ms.entity.config.MappingConfiguration;
 import com.icthh.xm.ms.entity.domain.XmEntity;
 import com.icthh.xm.ms.entity.repository.XmEntityRepositoryInternal;
 import com.icthh.xm.ms.entity.repository.search.XmEntitySearchRepository;
+import com.icthh.xm.ms.entity.service.search.ElasticsearchTemplateWrapper;
 import lombok.SneakyThrows;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
+import static org.mockito.ArgumentMatchers.any;
 import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.util.ReflectionUtils;
 
 import javax.persistence.EntityManager;
@@ -35,18 +35,20 @@ import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.Executors;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ElasticsearchIndexServiceUnitTest extends AbstractUnitTest {
 
-    @InjectMocks
     private ElasticsearchIndexService service;
+    private ElasticsearchTemplateWrapper elasticsearchTemplateWrapper;
     @Mock
     private XmEntityRepositoryInternal xmEntityRepository;
     @Mock
     private XmEntitySearchRepository xmEntitySearchRepository;
+
     @Mock
-    private ElasticsearchTemplateWrapper elasticsearchTemplateWrapper;
+    private ElasticsearchTemplate elasticsearchTemplate;
 
     @Mock
     private EntityManager entityManager;
@@ -59,20 +61,8 @@ public class ElasticsearchIndexServiceUnitTest extends AbstractUnitTest {
 
     @Before
     public void before() {
-        service.setSelfReference(service);
-    }
+        MockitoAnnotations.initMocks(this);
 
-    @Test
-    public void reindexAll() {
-        prepareInternal();
-
-        service.reindexAll();
-
-        verifyInternal();
-    }
-
-    @SneakyThrows
-    private void prepareInternal() {
         Class<XmEntity> entityClass = XmEntity.class;
         when(tenantContextHolder.getContext()).thenReturn(new TenantContext() {
             @Override
@@ -84,9 +74,26 @@ public class ElasticsearchIndexServiceUnitTest extends AbstractUnitTest {
                 return Optional.of(new PlainTenant(new TenantKey("XM")));
             }
         });
+
         when(xmEntityRepository.count(null)).thenReturn(10L);
         when(xmEntityRepository.findAll(null, PageRequest.of(0, 100))).thenReturn(
             new PageImpl<>(Collections.singletonList(createObject(entityClass))));
+
+        elasticsearchTemplateWrapper = new ElasticsearchTemplateWrapper(elasticsearchTemplate, xmEntitySearchRepository);
+
+        service = new ElasticsearchIndexService(
+            xmEntityRepository, elasticsearchTemplateWrapper, tenantContextHolder, mappingConfiguration,
+            indexConfiguration, Executors.newSingleThreadExecutor(), entityManager);
+
+        service.setSelfReference(service);
+    }
+
+    @Test
+    public void reindexAll() {
+
+        service.reindexAll();
+
+        verifyInternal();
     }
 
     @SneakyThrows
@@ -95,9 +102,9 @@ public class ElasticsearchIndexServiceUnitTest extends AbstractUnitTest {
 
         Class<XmEntity> entityClass = XmEntity.class;
 
-        verify(elasticsearchTemplateWrapper).deleteIndex("xm_xmentity");
-        verify(elasticsearchTemplateWrapper).createIndex("xm_xmentity");
-        verify(elasticsearchTemplateWrapper).putMapping(entityClass);
+        verify(elasticsearchTemplate).deleteIndex("xm_xmentity");
+        verify(elasticsearchTemplate).createIndex("xm_xmentity");
+        verify(elasticsearchTemplate).putMapping(entityClass);
 
         verify(xmEntityRepository, times(4)).count(any());
 
