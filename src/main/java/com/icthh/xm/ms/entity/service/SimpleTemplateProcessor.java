2b0aebc36ed493b89f283ca15e17760fe0ac7d70
Serhii Senko <serhii.senko@jevera.software>
2024-02-29 22:00:21 +0200

    Add description pattern

--- src/main/java/com/icthh/xm/ms/entity/service/SimpleTemplateProcessor.java
+++ src/main/java/com/icthh/xm/ms/entity/service/SimpleTemplateProcessor.java
@@ -3,6 +3,7 @@ package com.icthh.xm.ms.entity.service;
 import static com.jayway.jsonpath.Configuration.defaultConfiguration;
 import static com.jayway.jsonpath.Option.SUPPRESS_EXCEPTIONS;
 import static org.apache.commons.lang3.ObjectUtils.firstNonNull;
+import static org.apache.commons.text.StringSubstitutor.DEFAULT_VAR_DEFAULT;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.DocumentContext;
@@ -11,7 +12,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.text.StringSubstitutor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -25,12 +29,18 @@ public class SimpleTemplateProcessor {
         String json = objectMapper.writeValueAsString(object);
         DocumentContext document = JsonPath.using(defaultConfiguration().addOptions(SUPPRESS_EXCEPTIONS)).parse(json);
         StringSubstitutor sub = new StringSubstitutor();
+        sub.setValueDelimiter(UUID.randomUUID().toString());
         sub.setVariableResolver(key -> {
             String defaultValue = "";
-            if (key.contains(":")) {
-                defaultValue = key.split(":")[1];
+            if (key.contains(DEFAULT_VAR_DEFAULT)) {
+                String[] parts = key.split(DEFAULT_VAR_DEFAULT);
+                key = parts[0];
+                if (parts.length > 1) {
+                    defaultValue = parts[1];
+                }
             }
-            return String.valueOf(firstNonNull((Object) document.read(key), defaultValue));
+            Object value = document.read(key);
+            return value != null ? value.toString() : defaultValue;
         });
         String result = sub.replace(template);
         log.debug("Template {}, params {}, result {}", template, json, result);
