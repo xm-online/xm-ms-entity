4cf29b4caf3955c8fe90f63e2ef9d50a2a558dea
Gitlab CI/CD <gitlab@jevera.software>
2024-03-18 14:21:05 +0200

    Fix tests

--- src/test/java/com/icthh/xm/ms/entity/config/TestLepContext.java
+++
@@ -1,8 +0,0 @@
-package com.icthh.xm.ms.entity.config;
-
-import com.icthh.xm.commons.lep.processor.GroovyMap;
-import com.icthh.xm.ms.entity.lep.LepContext;
-
-@GroovyMap
-public class TestLepContext extends LepContext {
-}
--- src/test/java/com/icthh/xm/ms/entity/config/TestLepContextFactory.java
+++ src/test/java/com/icthh/xm/ms/entity/config/TestLepContextFactory.java
@@ -2,7 +2,6 @@ package com.icthh.xm.ms.entity.config;
 
 import com.icthh.xm.commons.lep.api.BaseLepContext;
 import com.icthh.xm.commons.lep.api.LepContextFactory;
-import com.icthh.xm.commons.lep.processor.GroovyMap;
 import com.icthh.xm.lep.api.LepMethod;
 import com.icthh.xm.ms.entity.lep.LepContext;
 import com.icthh.xm.ms.entity.service.XmEntityService;
@@ -15,7 +14,7 @@ public class TestLepContextFactory implements LepContextFactory {
 
     @Override
     public BaseLepContext buildLepContext(LepMethod lepMethod) {
-        TestLepContext testLepContext = new TestLepContext();
+        LepContext testLepContext = new LepContext();
         testLepContext.services.xmEntity = xmEntityService;
         return testLepContext;
     }
