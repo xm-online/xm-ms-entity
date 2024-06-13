2f9068dfc47a41fa5ce5d6be5d74a3ca5d34037b
bvolokhenko <bohdan.volokhonenko@jevera.software>
2024-06-13 13:06:49 +0300

    [VF-60] Added copyright license

--- src/main/java/com/icthh/xm/ms/entity/service/search/aggregation/Aggregations.java
+++ src/main/java/com/icthh/xm/ms/entity/service/search/aggregation/Aggregations.java
@@ -15,6 +15,8 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ *
+ * Original version of this file is located at: URL
  */
 package com.icthh.xm.ms.entity.service.search.aggregation;
 
