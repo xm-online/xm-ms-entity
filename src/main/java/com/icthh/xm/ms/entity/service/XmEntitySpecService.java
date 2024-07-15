53bad2b106c4cef1a07a10a1a01abfa2f5de4275
ssenko <gitlab@jevera.software>
2024-07-15 14:53:42 +0300

    add lazy to bean

--- src/main/java/com/icthh/xm/ms/entity/service/XmEntitySpecService.java
+++ src/main/java/com/icthh/xm/ms/entity/service/XmEntitySpecService.java
@@ -29,6 +29,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.util.AntPathMatcher;
 
@@ -53,6 +54,7 @@ import static org.springframework.util.CollectionUtils.isEmpty;
  * manipulate with pure model.
  */
 @Slf4j
+@Lazy
 @Service
 @RequiredArgsConstructor
 public class XmEntitySpecService implements RefreshableConfiguration {
