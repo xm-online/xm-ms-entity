73296b1e92a6a6292b6077de97bc0f683e015798
vosiievska <valentinaosievskaya@gmail.com>
2024-08-13 11:56:10 +0300

    cleaning

--- src/main/java/com/icthh/xm/ms/entity/config/LocaleConfiguration.java
+++ src/main/java/com/icthh/xm/ms/entity/config/LocaleConfiguration.java
@@ -1,10 +1,9 @@
 package com.icthh.xm.ms.entity.config;
 
-import java.nio.charset.Charset;
-import org.springframework.context.MessageSource;
+import com.icthh.xm.commons.i18n.spring.config.CommonMessageSourceConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,6 +11,7 @@ import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
+@Import({CommonMessageSourceConfiguration.class})
 public class LocaleConfiguration implements WebMvcConfigurer {
 
     @Bean(name = "localeResolver")
