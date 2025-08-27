a23e8dc1a09a2ab407033fb85386b8adb1dad7e6
ssenko <serhii.senko@jevera.software>
2025-08-27 10:20:27 +0300

    fix tests

--- src/test/java/com/icthh/xm/ms/entity/config/TestLepUpdateModeConfiguration.java
+++ src/test/java/com/icthh/xm/ms/entity/config/TestLepUpdateModeConfiguration.java
@@ -2,6 +2,7 @@ package com.icthh.xm.ms.entity.config;
 
 import com.icthh.xm.commons.config.client.repository.CommonConfigRepository;
 import com.icthh.xm.commons.config.client.repository.TenantListRepository;
+import com.icthh.xm.commons.config.client.service.CommonConfigService;
 import com.icthh.xm.commons.config.client.service.TenantAliasService;
 import com.icthh.xm.commons.config.client.service.TenantAliasServiceImpl;
 import com.icthh.xm.commons.lep.spring.LepUpdateMode;
@@ -22,6 +23,16 @@ public class TestLepUpdateModeConfiguration {
     @Bean
     public TenantAliasService tenantAliasService() {
         return new TenantAliasServiceImpl(mock(CommonConfigRepository.class), mock(TenantListRepository.class));
+    }
+
+    @Bean
+    public CommonConfigRepository commonConfigRepository() {
+        return mock(CommonConfigRepository.class);
+    }
+
+    @Bean
+    public CommonConfigService commonConfigService() {
+        return mock(CommonConfigService.class);
     }
 
 }
