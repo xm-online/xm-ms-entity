57430f74777d9850eda3ad0a1eca88f402c6bafc
Valentyna Osiievska <36424928+osiievska23@users.noreply.github.com>
2025-09-17 12:50:18 +0300

    added custom-privileges exactor functionality (#483)
    * added custom-privileges exactor functionality
    
    * autoincrement project version
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- src/main/java/com/icthh/xm/ms/entity/service/privileges/custom/ApplicationCustomPrivilegesExtractor.java
+++ src/main/java/com/icthh/xm/ms/entity/service/privileges/custom/ApplicationCustomPrivilegesExtractor.java
@@ -1,18 +1,19 @@
 package com.icthh.xm.ms.entity.service.privileges.custom;
 
+import com.icthh.xm.commons.permission.service.custom.CustomPrivilegesExtractor;
 import com.icthh.xm.ms.entity.domain.spec.TypeSpec;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 import static java.lang.Boolean.TRUE;
 import static java.util.stream.Collectors.toList;
 
 @Slf4j
 @Component
-public class ApplicationCustomPrivilegesExtractor implements CustomPrivilegesExtractor {
+public class ApplicationCustomPrivilegesExtractor implements CustomPrivilegesExtractor<TypeSpec> {
 
     private static final String APPLICATION_SECTION_NAME = "applications";
     private static final String APPLICATION_PRIVILEGE_PREFIX = "APPLICATION.";
@@ -28,10 +29,11 @@ public class ApplicationCustomPrivilegesExtractor implements CustomPrivilegesExt
     }
 
     @Override
-    public List<String> toPrivilegesList(Map<String, TypeSpec> specs) {
-        return specs.values().stream().filter(it -> TRUE.equals(it.getIsApp()))
-                                      .map(TypeSpec::getKey)
-                                      .collect(toList());
+    public List<String> toPrivilegesList(Collection<TypeSpec> specs) {
+        return specs.stream()
+            .filter(it -> TRUE.equals(it.getIsApp()))
+            .map(TypeSpec::getKey)
+            .collect(toList());
     }
 
 }
