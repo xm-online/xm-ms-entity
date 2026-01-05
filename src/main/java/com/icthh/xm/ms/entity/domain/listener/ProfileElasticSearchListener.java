f25b019e1b1d407cba1c3f2feaf495e11003a324
viktorpavlenko <vitek.pavfurious@gmail.com>
2026-01-05 12:05:38 +0200

    XM1-36012: Clean up;

--- src/main/java/com/icthh/xm/ms/entity/domain/listener/ProfileElasticSearchListener.java
+++ src/main/java/com/icthh/xm/ms/entity/domain/listener/ProfileElasticSearchListener.java
@@ -11,7 +11,6 @@ import jakarta.persistence.PostRemove;
 import jakarta.persistence.PostUpdate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Slf4j
