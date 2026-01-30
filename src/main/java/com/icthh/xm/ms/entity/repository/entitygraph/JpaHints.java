ec7e5f628915b0daa9e111146e3cf2bdf2154c72
Yaroslav <@>
2026-01-30 10:00:14 +0200

    Use jakarta.persistence.loadgraph instead of deprecated javax.persistence.loadgraph

---
+++ src/main/java/com/icthh/xm/ms/entity/repository/entitygraph/JpaHints.java
@@ -0,0 +1,9 @@
+package com.icthh.xm.ms.entity.repository.entitygraph;
+
+public final class JpaHints {
+
+    public static final String LOAD_GRAPH = "jakarta.persistence.loadgraph";
+
+    private JpaHints() {
+    }
+}
