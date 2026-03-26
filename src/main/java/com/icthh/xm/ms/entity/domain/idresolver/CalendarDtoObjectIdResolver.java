b8e3085b16ea1e4de79a705d787141d14181acfe
viktorpavlenko <vitek.pavfurious@gmail.com>
2026-03-26 13:16:39 +0200

    XM1-35887: Fix trouble with inject

--- src/main/java/com/icthh/xm/ms/entity/domain/idresolver/CalendarDtoObjectIdResolver.java
+++ src/main/java/com/icthh/xm/ms/entity/domain/idresolver/CalendarDtoObjectIdResolver.java
@@ -6,9 +6,10 @@ import com.fasterxml.jackson.annotation.SimpleObjectIdResolver;
 import com.icthh.xm.commons.exceptions.BusinessException;
 import com.icthh.xm.commons.exceptions.ErrorConstants;
 import com.icthh.xm.ms.entity.repository.CalendarRepository;
+import com.icthh.xm.ms.entity.repository.XmEntityRepository;
 import com.icthh.xm.ms.entity.service.dto.CalendarDto;
+import com.icthh.xm.ms.entity.util.AutowireHelper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -22,19 +23,13 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class CalendarDtoObjectIdResolver extends SimpleObjectIdResolver {
 
-    private CalendarRepository repository;
-
-    @Autowired
-    public CalendarDtoObjectIdResolver(CalendarRepository repository) {
-        this.repository = repository;
-    }
-
     public CalendarDtoObjectIdResolver() {
         log.debug("CalendarDto object id resolver inited");
     }
 
     @Override
     public Object resolveId(final ObjectIdGenerator.IdKey id) {
+        CalendarRepository repository = getRepository();
         var entity = repository.findById((Long) id.key).orElseThrow(
             () -> new BusinessException(ErrorConstants.ERR_NOTFOUND,
                 "Can not resolve Calendar by ID: " + id.key));
@@ -44,8 +39,12 @@ public class CalendarDtoObjectIdResolver extends SimpleObjectIdResolver {
         return dto;
     }
 
+    private CalendarRepository getRepository() {
+        return AutowireHelper.getInstance().getBean(CalendarRepository.class);
+    }
+
     @Override
     public ObjectIdResolver newForDeserialization(final Object context) {
-        return new CalendarDtoObjectIdResolver(repository);
+        return new CalendarDtoObjectIdResolver();
     }
 }
