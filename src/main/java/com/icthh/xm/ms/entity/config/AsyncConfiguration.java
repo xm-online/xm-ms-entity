b7137c9d0102e4400c00576be5f7f3624ef910ff
viktorpavlenko <vitek.pavfurious@gmail.com>
2026-04-30 13:04:41 +0300

    Clean up;

--- src/main/java/com/icthh/xm/ms/entity/config/AsyncConfiguration.java
+++ src/main/java/com/icthh/xm/ms/entity/config/AsyncConfiguration.java
@@ -5,7 +5,6 @@ import java.util.concurrent.Executors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
